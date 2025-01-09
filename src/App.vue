ffa51217018fe3641ee0ede227e19f5300b51667
Victor Georges <victorgeorges54@gmail.com>
2025-01-09 14:35:45 +0100

    :tada: Init du projet

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/login">Home</RouterLink>
+        <RouterLink to="/register">About</RouterLink>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+  <Dashboard />
+</template>
